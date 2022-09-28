186f6591db9a7d9faf5f69d4caa1ece1c9a10f97
BT_3W2_33 <nathanz@nycstudents.net>
2022-09-28 08:42:31 -0400

    added a printed statement where a number is rounded

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String [] args)
+    {
+        System.out.println("Hello World!");
+        int x = 23;
+        x+=3;
+        System.out.println(x);
+        double change = 342.34;
+        double newChange = Math.round(change);
+        System.out.println(newChange);
+    }
 }
